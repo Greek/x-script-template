e92d3527f9df878b337f7976dee2864e30857dfc
Andreas <me@apap04.com>
2022-08-07 10:42:23 -0400

    refactor: remove stuff

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Client from './core/Client';
-const client = new Client('Hello World', 9);
-
-const argument = 'This is unused. Can eslint catch it? I think not!';
-
-console.log(`${client.programName} ${client.token}`);
-client.doSomething('my argument');
